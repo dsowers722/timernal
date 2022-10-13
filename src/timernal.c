6df28ad57ada6e4d74e93f5fe9df99c79e8b44e0
Daniel Sowers <danielsowers722@gmail.com>
2022-10-13 17:56:18 -0400

    Minor changes

--- src/timernal.c
+++ src/timernal.c
@@ -1,5 +1,14 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-void main() {
-	printf("Hello world\n");
+char *time;
+char *characters[10];
+
+int main(int argc, char* argv[]) {
+	time = argv[1];
+	FILE *file = fopen("./numbers.txt", "r");
+	// TODO fix the file reading issues.
+	fscanf(file, "  %c", characters[0]);
+	printf("%s", characters[0]);
+	return 1;
 }
