08aa24bfcb2133c11cc3f1531437255e9d4bcfe8
Daniel Sowers <danielsowers722@gmail.com>
2022-10-18 00:22:30 -0400

    Added improvements

--- src/timernal.c
+++ src/timernal.c
@@ -12,12 +12,18 @@ const char *characters[][5] = {{" 0000 ", "00  00", "00  00", "00  00", " 0000 "
 								{"777777", "   77 ", "  77  ", " 77   ", "77    "},
 								{" 8888 ", "88  88", " 8888 ", "88  88", " 8888 "},
 								{" 9999 ", "99  99", " 99999", "    99", " 9999 "}};
+const char *dash[5] = {"  ", "  ", "--", "  ", "  "};
 
 void print_time(int h, int m, int s) {
+	//first digit = num / 10
+	//second digit = num % 10
 	for (int i = 0; i < 5; i++) {
-		printf("%s\n", characters[h][i]);
+		printf("%s    %s    %s    %s    %s    %s    %s    %s\n",
+			   characters[h / 10][i], characters[h % 10][i], dash[i],
+			   characters[m / 10][i], characters[m % 10][i], dash[i],
+			   characters[s / 10][i], characters[s % 10][i]);
 	}
-}
+} //The job of this function is to only handle the printing correctly of the paraeters passed.
 
 void timer(int h, int m, int s) {
 	if (h == -1 && m == -1 && s == -1) return;
@@ -25,9 +31,11 @@ void timer(int h, int m, int s) {
 	print_time(h, m, s);
 	sleep(1);
 	timer(h-1,m-1,s-1);
-}
+} // Pass the correct time values into the printing function and handle the mechanics of a timer.
 
 void main(int argc, char* argv[]) {
 	timer(atoi(argv[1]) / 10000, (atoi(argv[1]) % 10000) / 100, atoi(argv[1]) % 100);
+	system("clear");
+	printf("Completed\n");
 	return;
-}
+} // main function
